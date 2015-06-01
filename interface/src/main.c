58db5b264833fe3092ed83d1f1a4b75d3d195794
kubov <jakub@kubiak.es>
2015-06-01 21:22:06 +0200

    Add more function abstraction.
    Signed-off-by: Jakub Kubiak <jakub@kubiak.es>

--- interface/src/main.c
+++ interface/src/main.c
@@ -37,7 +37,51 @@ uint8_t SPI(uint8_t TX_Data) {
 	return LPC_SSP->DR;
 }
 
-uint8_t mirf_init()
+uint8_t nrf_read_register(uint8_t reg, uint8_t *data, uint8_t len)
+{
+      SSEL_LOW;
+      uint8_t r = SPI(reg);
+
+      for (int i=0; i<len; i++)
+          data[i] = SPI(0xff); // send dummy byte
+      SSEL_HIGH;
+
+      return r;
+}
+
+uint8_t nrf_write_register(uint8_t reg, const uint8_t *data, uint8_t len)
+{
+    SSEL_LOW;
+    uint8_t r = SPI(0x20 | (0x1f & reg));
+
+    for (int i=0; i<len; i++)
+        SPI(data[i]);
+    SSEL_HIGH;
+
+    return r;
+}
+
+void nrf_setup(uint8_t channel,
+               uint8_t payload_size,
+               const uint8_t* raddr,
+               const uint8_t* taddr)
+{
+    // set channel
+    nrf_write_register(RF_CH, &channel, 1);
+
+    // set payload size
+    nrf_write_register(RX_PW_P0, &payload_size, 1);
+
+    // set taddr
+    nrf_write_register(TX_ADDR, taddr, 5);
+
+    // set raddr
+    CE_LOW;
+    nrf_write_register(RX_ADDR_P0, raddr, 5);
+    CE_HIGH;
+}
+
+void interface_setup()
 {
     // set SSEL
     GPIOSetDir(SSEL_PORT, SSEL, 1);
@@ -54,45 +98,30 @@ uint8_t mirf_init()
 
     // start SSP
     SSPInit();
-}
 
-uint8_t mirf_read_register(uint8_t reg, uint8_t *data, uint8_t len)
-{
-      SSEL_LOW;
-      SPI(reg);
+    // start uart
+    UARTInit(115200);
 
-      for (int i=0; i<len; i++)
-          data[i] = SPI(0xff); // send dummy byte
-      SSEL_HIGH;
+    // setup timer
+    init_timer32(0, TIME_INTERVAL);
+    enable_timer32(0);
 
-      return data[0];
-}
 
-void mirf_write_register(uint8_t reg, uint8_t *data, uint8_t len)
-{
-    SSEL_LOW;
-    SPI(0x20 | (0x1f & reg));
-
-    for (int i=0; i<len; i++)
-        SPI(data[i]);
-    SSEL_HIGH;
 }
 
 int main (void)
 {
     char status = 0xff;
     char value = 0xcc;
-
-    init_timer32(0, TIME_INTERVAL);
-    enable_timer32(0);
-    mirf_init();
-    UARTInit(115200);
-
-    mirf_write_register(0x0a, "wlan0", 5);
     uint8_t buf[5];
+
+    interface_setup();
+
+
+    nrf_setup(5, 32, "raddr", "taddr");
     while (1)
     {
-        mirf_read_register(0x0a, buf, 5);
+        nrf_read_register(0x0a, buf, 5);
         delay32Ms(0, 500);
         //__WFI();
     }
